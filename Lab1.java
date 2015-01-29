7761c702ed90720c2a3e4efc46e593c8b00c6cad
Dennis Bennhage <bennhage@ed3354-14.ed3354.studat.rh6.ii.2.hb.s.cdal.chalmers.se>
2015-01-29 11:58:29 +0100

    Further work done on lab session

--- Lab1.java
+++ Lab1.java
@@ -1,17 +1,14 @@
-import TSim.SensorEvent;
-
 public class Lab1 {
 	
 	private static final int maxSpeed = 50;
-	private static int simSpeed       = 100;
 	
 	public static void main(String[] args) {
 		new Lab1(args);
 	}
 
-	@SuppressWarnings("static-access")
 	public Lab1(String[] args) {
 		int t1speed = maxSpeed, t2speed = maxSpeed;
+		int simSpeed = 100;
 		
 		switch (args.length) {
 		default:
@@ -27,40 +24,10 @@ public class Lab1 {
 		
 		Train t1 = new Train(1,t1speed);
 		Train t2 = new Train(2,t2speed);
-		t1.run(); t2.run();
-		
-		SensorEvent active;
+		Train.simSpeed = simSpeed;
 		while(true) {
-			active = t1.getActiveSensor();
-			if (active != null) {
-				if (t1.isCritical(active)) {
-					t1.request(t1.getSectionNumber(active));
-				} else if (t1.isStation(active)) {
-					t1.brake();
-					try {
-						t1.sleep(2 + 2 * simSpeed * Math.abs(t1speed));
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					t1.reverse();
-					t1.run();
-				}
-			}
-			active = t2.getActiveSensor();
-			if (active != null) {
-				if (t2.isCritical(active)) {
-					t2.request(t2.getSectionNumber(active));
-				} else if (t2.isStation(active)) {
-					t2.brake();
-					try {
-						t2.sleep(2 + 2 * simSpeed * Math.abs(t2speed));
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					t2.reverse();
-					t2.run();
-				} 
-			}
+			t1.checkEnvironment();
+			t2.checkEnvironment();
 		}
 	}
 }
--- Train.java
+++ Train.java
@@ -12,9 +12,13 @@ import static TSim.SensorEvent.*;
  */
 public class Train extends Thread {
 
+	public static int simSpeed;
+	
 	private int id;
 	private int speed;
 	private TSimInterface tsi;
+	private Boolean isAtStation = true;
+	private Boolean isInCritical = false;
 	
 	private static final Semaphore[] sems = new Semaphore[]{
 		new Semaphore(1),
@@ -38,11 +42,27 @@ public class Train extends Thread {
 	 */
 	public final SensorEvent[] criticals =
 			new SensorEvent[]{
+				// Critical section 1
 				new SensorEvent(id,6,6,INACTIVE),
-				new SensorEvent(id,11,8,INACTIVE),
-				new SensorEvent(id,14,7,INACTIVE),
-				new SensorEvent(id,12,9,INACTIVE),
-				new SensorEvent(id,7,10,INACTIVE)
+				new SensorEvent(id,8,5,INACTIVE),
+				new SensorEvent(id,10,8,INACTIVE),
+				new SensorEvent(id,11,7,INACTIVE),
+				// Critical section 2					
+				new SensorEvent(id,14,7,INACTIVE),	
+				new SensorEvent(id,15,8,INACTIVE),	
+				new SensorEvent(id,19,9,INACTIVE),
+				// Critical section 3					
+				new SensorEvent(id,18,9,INACTIVE),	
+				new SensorEvent(id,12,9,INACTIVE),	
+				new SensorEvent(id,13,10,INACTIVE),
+				// Critical section 4
+				new SensorEvent(id,7,9,INACTIVE),	
+				new SensorEvent(id,6,10,INACTIVE),	
+				new SensorEvent(id,1,9,INACTIVE),
+				// Critical section 5
+				new SensorEvent(id,1,10,INACTIVE),	
+				new SensorEvent(id,6,11,INACTIVE),	
+				new SensorEvent(id,4,13,INACTIVE),
 	};
 	
 	/** Creates a new instance of a train.
@@ -54,6 +74,7 @@ public class Train extends Thread {
 		tsi = TSimInterface.getInstance();
 		this.id = id;
 		this.speed = speed;
+		run();
 	}
 	
 	/** Starts the train with its specified speed.
@@ -97,15 +118,20 @@ public class Train extends Thread {
 	 */
 	public int getSectionNumber(SensorEvent e) {
 		int i = -1;
-		if (criticals[0] == e) {
+		if (sensorEqual(e, criticals[0]) || sensorEqual(e, criticals[1]) || 
+				sensorEqual(e, criticals[2]) || sensorEqual(e, criticals[3])) {
 			i = 0;
-		} else if (criticals[1] == e) {
+		} else if (sensorEqual(e, criticals[4]) ||
+				sensorEqual(e, criticals[5]) || sensorEqual(e, criticals[6])) {
 			i = 1;
-		} else if (criticals[2] == e) {
+		} else if (sensorEqual(e, criticals[7]) || 
+				sensorEqual(e, criticals[8]) || sensorEqual(e, criticals[9])) {
 			i = 2;
-		} else if (criticals[3] == e) {
+		} else if (sensorEqual(e, criticals[10]) || 
+				sensorEqual(e, criticals[11]) || sensorEqual(e, criticals[12])) {
 			i = 3;
-		} else if (criticals[4] == e) {
+		} else if (sensorEqual(e, criticals[13]) || 
+				sensorEqual(e, criticals[14]) || sensorEqual(e, criticals[15])) {
 			i = 4;
 		}
 		return i;
@@ -119,41 +145,61 @@ public class Train extends Thread {
 	public int getStationNumber(SensorEvent e) {
 		// The number of stations is 4.
 		for (int i = 0; i < 4; i++) {
-			if (e == stations[i]) return i;
+			if (sensorEqual(e,stations[i])) return i;
 		}
 		return -1;	// Returns -1 if 'e' isn't in 'stations'.
 	}
 	
-	/** Checks whether an instance of an SensorEvent is contained in 'criticals'.
+	/** Retrieves the current active sensor. If none is active, then null is
+	 *  returned.
 	 * 
-	 *  @param SensorEvent e, reference to an SensorEvent.
+	 *  @return A SensorEvent.
+	 */
+	public SensorEvent getActiveSensor() {
+		SensorEvent s = null;
+		try {
+			s = tsi.getSensor(id);
+		} catch (CommandException | InterruptedException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		return s;
+	}
+	
+	/** Checks whether an instance of a SensorEvent is contained in 'criticals'.
+	 * 
+	 *  @param SensorEvent e, reference to a SensorEvent.
 	 *  @return true if 'criticals' contains instance 'e', false otherwise.
 	 */
 	public Boolean isStation(SensorEvent e) {
-		return isSensor(e, stations);
+		return isSensor(e, stations); 
 	}
 	
-	/** Checks whether an instance of an SensorEvent is contained in 'stations'.
+	/** Checks whether an instance of a SensorEvent is contained in 'stations'.
 	 * 
-	 *  @param SensorEvent e, reference to an SensorEvent.
+	 *  @param SensorEvent e, reference to a SensorEvent.
 	 *  @return true if 'stations' contains instance 'e', false otherwise.
 	 */
 	public Boolean isCritical(SensorEvent e) {
-		return isSensor(e, criticals);
+		return isSensor(e, criticals); 
 	}
 	
-	/** Checks whether an instance of an SensorEvent is contained in a list 
+	/** Checks whether an instance of a SensorEvent is contained in a list 
 	 *  of SensorEvents.
 	 * 
-	 *  @param SensorEvent e, reference to an SensorEvent.
+	 *  @param SensorEvent e, reference to a SensorEvent.
 	 *  @param SensorEvent[] sensors, a list of sensors.
 	 *  @return true if 'sensors' contains instance 'e', false otherwise.
 	 */
 	private Boolean isSensor(SensorEvent e, SensorEvent[] sensors) {
 		for (SensorEvent sensor : sensors) {
-			if (e == sensor) return true;
+			if (sensorEqual(e, sensor)) return true;
 		}
 		return false;
+	}
+	
+	private Boolean sensorEqual(SensorEvent e1, SensorEvent e2) {
+		return e1.getXpos() == e2.getXpos() && e1.getYpos() == e2.getYpos();
 	}
 	
 	/** Requests to pass through a critical section by asking the semaphore of
@@ -193,19 +239,37 @@ public class Train extends Thread {
 		
 		sems[sectionNumber].release();
 	}
-
-	/** Retrieves the current active sensor. If none is active, then null is
-	 *  returned.
-	 * 
-	 *  @return A SensorEvent.
-	 */
-	synchronized public SensorEvent getActiveSensor() {		
-		for (SensorEvent s : criticals) {
-			if (s.getStatus() == ACTIVE) return s;
+	
+	public void checkEnvironment() {
+		SensorEvent active = getActiveSensor();
+		if (active != null) {
+			if (isCritical(active)) {
+				if (!isInCritical) {
+					System.err.println("Train " + id +" entering cs");
+					request(getSectionNumber(active));
+					isInCritical = true;
+				} else {
+					System.err.println("Train " + id +" exiting cs");
+					signal(getSectionNumber(active));
+					isInCritical = false;
+				}
+			} else if (isStation(active)) {
+				if (!isAtStation) {
+					System.err.println("Train " + id +" entering station");
+					isAtStation = true;
+					brake();
+					try {
+						sleep(2000 + 2 * simSpeed * Math.abs(speed));
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					reverse();
+					run();
+				} else {
+					System.err.println("Train " + id +" exiting station");
+					isAtStation = false;
+				}
+			}
 		}
-		for (SensorEvent s : stations) {
-			if (s.getStatus() == ACTIVE) return s;
-		}
-		return null;
 	}
 }
