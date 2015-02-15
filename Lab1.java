aa579d6d394ba01303501342800bdc7e7c264347
Hampus L. (OSX) <hampuslidin@gmail.com>
2015-02-15 17:53:09 +0100

    Added Lab2.java

--- Lab1.java
+++ Lab1.java
@@ -1,7 +1,7 @@
 public class Lab1 {
-	
+
 	private static final int maxSpeed = 20;
-	
+
 	public static void main(String[] args) {
 		new Lab1(args);
 	}
@@ -9,11 +9,11 @@ public class Lab1 {
 	public Lab1(String[] args) {
 		int t1speed = maxSpeed, t2speed = maxSpeed;
 		int simSpeed = 100;
-		
+
 		switch (args.length) {
 		default:
-		case 3: 
-			simSpeed = Integer.parseInt(args[2]); 
+		case 3:
+			simSpeed = Integer.parseInt(args[2]);
 		case 2:
 			t2speed = Integer.parseInt(args[1]);
 			if (t2speed > maxSpeed) t2speed = maxSpeed;
@@ -23,9 +23,9 @@ public class Lab1 {
 		case 0:
 			break;
 		}
-		
-		Train2 t1 = new Train2(1, t1speed, simSpeed);
-		Train2 t2 = new Train2(2, t2speed, simSpeed);
+
+		Train t1 = new Train(1, t1speed, simSpeed);
+		Train t2 = new Train(2, t2speed, simSpeed);
 		t1.start();
 		t2.start();
 	}
